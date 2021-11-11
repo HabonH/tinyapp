1b2179cd8ea213200a897064fb9e5f8f214f9de6
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-11 17:17:31 -0500

    Update with new urlDatabase

--- express_server.js
+++ express_server.js
@@ -23,9 +23,20 @@ const users = {
   }
 };
 
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
 };
 
 const findUserByEmail = (userEmail) => {          // Email helper function, to see if a user has existing email registered
@@ -37,8 +48,7 @@ const findUserByEmail = (userEmail) => {          // Email helper function, to s
   return null;
 };
 
-
-
+// --- Generates a random alphanumeric string
 function generateRandomString() {
   let newID = Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);;
   return newID;
@@ -56,84 +66,130 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//--- Allows clients to create a shortURL if they are logged in, then redirects them to that new shortURL page after adding longURL + userID to urlDatabase
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  const newShortURL = generateRandomString();
-  // console.log(newShortURL);
-  urlDatabase[newShortURL] = req.body.longURL;
-  // console.log(urlDatabase);
-  res.redirect(`/urls/${newShortURL}`);
+  // console.log("req.body---> ", req.body);  // Log the POST request body to the console
+  if (!users[req.cookies.user_id]) {
+    res.redirect("/login");
+  } else if (users[req.cookies.user_id]) {
+    const newShortURL = generateRandomString();
+    const {longURL} = req.body;
+    urlDatabase[newShortURL] = {longURL, userID: req.cookies.user_id}
+
+    // console.log("shortURL--> ", newShortURL);
+    // console.log("longURL--> ", longURL);
+    // console.log("Updated URL database ", urlDatabase)
+    
+    res.redirect(`/urls/${newShortURL}`);
+  }
 });
 
-
+// --- Allows clients to view existing shortURL and longURL. *** Can't edit and can
 app.get("/urls", (req, res) => {
   const templateVars = {
     user: users[req.cookies.user_id],
     urls: urlDatabase
   };
+  // console.log("urlDatabase --->", urlDatabase)
   res.render("urls_index", templateVars);
 });
 
-
+// --- Takes client to url/new page but must be logged in in order to create new URL
 app.get("/urls/new", (req, res) => {
   // const userID = req.cookies.user_id; --- don't remove these comments, they help for self readibility
   // const user = users[userID];
   // const templateVars = {
   //   user,
   // };
-  const templateVars = {
-    user: users[req.cookies.user_id]
-  };
-  res.render("urls_new", templateVars);
+  if (!users[req.cookies.user_id]) {
+    res.cookie("error", "Please login or register to create a new URL");
+    res.redirect("/login");
+  } else if (users[req.cookies.user_id]) {
+    const templateVars = {
+      user: users[req.cookies.user_id]
+    };
+    res.render("urls_new", templateVars);
+  }
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  // console.log("Req param result, ", req.params); 
-  const templateVars = { user: users[req.cookies.user_id], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-});
-
+// --- Redirects client to existing longURL via shortURL link *** longURL not working
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL;
+  const shortURL = req.params.shortURL;
+  // console.log("shortURL ---> ", shortURL)
+  const longURL = urlDatabase[shortURL].longURL;
+  // console.log("longURL ---> ", longURL);
   res.redirect(longURL);
 });
 
+// --- Allows clients to view shortURLs with assigned longURL. *** longURL not being displayed in urls_shows & url not dis
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  // console.log("shortURL variable ---> ", shortURL);
+  const longURL = urlDatabase[shortURL].longURL;
+  const userID = urlDatabase[shortURL].userID;
+  // console.log("longUrl ---> ", longURL);
+  const templateVars = { user: users[req.cookies.user_id], shortURL: shortURL, longURL: longURL, userID: userID };
+  res.render("urls_show", templateVars);
+});
+
+
+// --- Allows clients to delete existing shortURL
 app.post("/urls/:shortURL/delete", (req, res) => {
+  if (!users[req.cookies.user_id]) {
+    res.cookie("error", "Please login to delete URL");
+    res.redirect("/login");
+  
+  } else if (users[req.cookies.user_id]) {
   const shortURL = req.params.shortURL;
   // const longURL= urlDatabase[req.params.shortURL];
   delete urlDatabase[shortURL];
   res.redirect("/urls");
+  }
 });
 
-app.post("/urls/:shortURL", (req, res) => {
+// --- Allows clients create a new shortURL with long URL, then redirect to /urls page
+app.post("/urls/:shortURL", (req, res) => { //--------------Not editing existing url
+  if (!users[req.cookies.user_id]) {
+    res.cookie("error", "Please login to edit URL");
+    res.redirect("/login");
+  
+  } else if (users[req.cookies.user_id]) {
   const shortURL = req.params.shortURL;
-  const longURL = req.body.longURL;
+  const longURL = urlDatabase[shortURL].longURL;
   urlDatabase[shortURL].longURL = longURL;
   res.redirect("/urls");
+
+  }
 });
 
+// --- Allows clients to get/read to the login page 
 app.get("/login", (req, res) => {
   const templateVars = {
-    user: users[req.cookies.user_id]
+    user: users[req.cookies.user_id],
+    email: req.cookies.email,
+    error_msg: req.cookies.error
   };
+  res.clearCookie("error");
   res.render("urls_login", templateVars);
 });
 
+
+// --- Allows clients to send post requests to login, as long as email and password exists and matches in users database
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   if (email === '' || password === '') {
     return res.status(400).send("To login, please provide your e-mail and password");
   }
-  const userExists = findUserByEmail(email);
-  if (!userExists) {
+  const user = findUserByEmail(email);
+  if (!user) {
     return res.status(403).send(`User associated with ${email} doesn't exist, please register`);
 
-  } else if (userExists) {
-    console.log("User does exist ---> ", userExists);
-    // I need to compare if userExist's password matches with the req.body.password
-    if (userExists.password === password) {
-      res.cookie("user_id", userExists.id);
+  } else if (user) {
+    // console.log("User does exist ---> ", user);
+    // I need to compare if user's password matches with the req.body.password
+    if (user.password === password) {
+      res.cookie("user_id", user.id);
       res.redirect("/urls");
     } else {
       return res.status(403).send("Invalid password");
@@ -141,11 +197,13 @@ app.post("/login", (req, res) => {
   }
 });
 
+// --- Allows clients to log out and it clears cookies upon logout
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
+// --- Allows clients to view registration page 
 app.get("/register", (req, res) => {
   const templateVars = {
     user: users[req.cookies.user_id],
@@ -154,12 +212,13 @@ app.get("/register", (req, res) => {
   res.render("urls_register", templateVars);
 });
 
+// --- Allows clients to register accurately according to conditions
 app.post("/register", (req, res) => {
   const id = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
 
-  const user = {
+  const newUser = {
     id: id,
     email: email,
     password: password
@@ -169,17 +228,17 @@ app.post("/register", (req, res) => {
     return res.status(400).send("To register, please provide your e-mail and password");
   }
 
-  const userExists = findUserByEmail(email);
-  if (userExists) {
+  const user = findUserByEmail(email);
+  if (user) {
     return res.status(400).send(`User with ${email} is already registered`);
   } else {
-    users[user.id] = user;
-    res.cookie('user_id', user.id);
+    users[newUser.id] = newUser;
+    res.cookie('user_id', newUser.id);
     res.redirect("/urls");
   }
   // console.log("user_id --> ", user.id);
   // console.log("id --> ", id);
-  console.log("Did error get logged---> ", users);
+  // console.log("Did error get logged---> ", users);
 });
 
 app.listen(PORT, () => {
